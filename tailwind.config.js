66cfa142ac35730dc402a96408215d84ffe55764
Shams Shahyar <shamsshahyar6@gmail.com>
2026-05-24 16:35:30 +0430

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"],
+    darkMode : 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
